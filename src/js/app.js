aa4dac67bce04de9f76f9ecddfa2aca53336f8c7
mr <mr.scrpt@gmail.com>
2023-02-08 11:50:10 +0200

    kit works

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,4 @@
 import './module/input.js'
+import './module/textarea.js'
 import './module/checkbox.js'
 import './module/radio.js'
--- src/js/module/textarea.js
+++ src/js/module/textarea.js
@@ -1,36 +1,51 @@
-import $ from 'jquery';
-import { makeId } from '../modules/functions.js';
+import $ from 'jquery'
+import { makeId } from './utils.js'
 
 /**************/
 /*Focuse State*/
 /**************/
 $(document).ready(() => {
-	const $textarea = $('.textarea');
-	$textarea.on('focus', ({ target }) => {
-		const $item = $(target.closest('.textarea'));
-		$item.addClass('textarea_focused');
-	});
+  const $textarea = $('.textarea')
+  $textarea.on('focus', ({ target }) => {
+    const $item = $(target.closest('.textarea'))
+    $item.addClass('textarea_state_focus')
+  })
 
-	$textarea.on('blur', ({ target }) => {
-		const $item = $(target.closest('.textarea'));
-		$item.removeClass('textarea_focused');
-	});
-});
+  $textarea.on('blur', ({ target }) => {
+    const $item = $(target.closest('.textarea'))
+    $item.removeClass('textarea_state_focus')
+  })
+})
 
+/**************/
+/*Hover State*/
+/**************/
+$(() => {
+  const $inputs = $('.textarea')
+  $inputs.on('mouseover', ({ target }) => {
+    const $item = $(target.closest('.textarea'))
+    $item.addClass('textarea_state_hover')
+  })
+
+  $inputs.on('mouseleave', ({ target }) => {
+    const $item = $(target.closest('.textarea'))
+    $item.removeClass('textarea_state_hover')
+  })
+})
 /*******************/
 /*Textarea with Label*/
 /*******************/
 $(document).ready(() => {
-	const $labels = $('.label');
+  const $labels = $('.label')
 
-	$labels.each((_, item) => {
-		const $labelCurrent = $(item);
-		const $textarea = $labelCurrent.find('.textarea');
-		if ($textarea.length) {
-			const idCustom = makeId(5);
+  $labels.each((_, item) => {
+    const $labelCurrent = $(item)
+    const $textarea = $labelCurrent.find('.textarea')
+    if ($textarea.length) {
+      const idCustom = makeId(5)
 
-			$labelCurrent.attr('for', idCustom);
-			$textarea.attr('id', idCustom);
-		}
-	});
-});
+      $labelCurrent.attr('for', idCustom)
+      $textarea.attr('id', idCustom)
+    }
+  })
+})
